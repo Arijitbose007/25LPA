514e03355d85fe9a03c99145c649aa168bf92b9e
Arijit Bose <arijitbose0007@gmail.com>
2024-04-05 20:00:54 +0530

    Day 7

--- Day 7/InsertnDeleteLL.cpp
+++ Day 7/InsertnDeleteLL.cpp
@@ -89,5 +89,6 @@ int main()
   
   head2 = nullptr;
   return 0;
+
 }
 
--- Day 7/gcd.cpp
+++ Day 7/gcd.cpp
@@ -7,25 +7,7 @@ int gcd(int a, int b) {
     return gcd(b, a % b);
 }
 
-int main() {
-    int num1, num2;
-    std::cout << "Enter two numbers: ";
-    std::cin >> num1 >> num2;
-    int result = gcd(num1, num2);
-    std::cout << "GCD: " << result << std::endl;
-    return 0;
-}
 int lcm(int a, int b) {
-    int gcd = gcd(a, b);
-    int lcm = (a * b) / gcd;
-    return lcm;
-}
-
-int main() {
-    int num1, num2;
-    std::cout << "Enter two numbers: ";
-    std::cin >> num1 >> num2;
-    int result = lcm(num1, num2);
-    std::cout << "LCM: " << result << std::endl;
-    return 0;
+    int gcdVal = gcd(a, b);
+    return (a * b) / gcdVal;
 }
